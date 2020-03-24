a89e7b5d884e5fa77a265cc0b580b40568f894ce
Arturo Villa <villa@MacBook-Air-de-Arturo.local>
2020-03-23 18:56:38 -0600

    Fix

--- Grafo.py
+++ Grafo.py
@@ -1,9 +1,9 @@
 #Primero construiremos el grafo del mapa, estamos usando el mapa de la ciudad de Rumania y el objetivo es 
 #llegar a la ciudad de Bucarest.
 
-grafoB = {'Arad':[('Timisoara',118),('Sibiu',140),('Zerind',75)],
+Grafo = {'Arad':[('Timisoara',118),('Sibiu',140),('Zerind',75)],
         'Timisoara':[('Lugoj',111),('Arad',118)],
-        'Sibiu':[('Rimnicu Vilcea',80),('Fagaras',99),('Oradea',151)],
+        'Sibiu':[('Rimnicu Vilcea',80),('Fagaras',99),('Oradea',151),('Arad',140)],
         'Zerind':[('Oradea',71),('Arad',75)],
         'Lugoj':[('Mehadia',70),('Timisoara',111)],
         'Rimnicu Vilcea':[('Craiova',146),('Pitesti',97),('Sibiu',80)],
@@ -22,44 +22,3 @@ grafoB = {'Arad':[('Timisoara',118),('Sibiu',140),('Zerind',75)],
         'Hirsova':[('Urziceni',98),('Eforie',86)],
         'Eforie':[('Hirsova',86)]
         }
-
-graph = {
-  'A' : ['B','C'],
-  'B' : ['D', 'E'],
-  'C' : ['F'],
-  'D' : [],
-  'E' : ['F'],
-  'F' : []
-}
-
-visitados = [] # List to keep track of visited nodes.
-cola = []     #Initialize a queue
-camino = []
-
-def bfs(visited, grafo, llave_ciudad, meta):
-  visitados.append(llave_ciudad)
-  cola.append(llave_ciudad)
-  encontrado = False
-
-  while cola and not encontrado:
-    nodo_actual = cola.pop(0)
-    camino.append(nodo_actual)
-
-    for neighbour,distancia in grafo[nodo_actual]:
-
-      if neighbour not in visited:
-         visited.append(neighbour)
-         cola.append(neighbour)
-      
-      if neighbour == meta:
-        encontrado = True
-        break
-
-    
-
-if __name__ == '__main__':
-    bfs(visitados, grafoB, 'Arad','Bucarest')
-    print(camino)
-    print()
-
-    print(visitados)
--- PrimeroAmplitud.py
+++ PrimeroAmplitud.py
@@ -1,33 +1,21 @@
 from Grafo import Grafo
 from Arbol import Arbol
 
-def busqueda_en_amplitud(grafo, nodo_inicial, nodo_meta):
-    nodos_visitados = []
-    cola_de_busqueda = []
+visitados = []
+cola = []
+def amplitud(visitados, grafo, llave_ciudad, meta):
+  visitados.append(llave_ciudad)
+  cola.append(llave_ciudad)
+  encontrado = False
 
-    cola_de_busqueda.append(nodo_inicial)
+  while cola and not encontrado:
+    nodo_actual = cola.pop(0)
 
-    while cola_de_busqueda:
+    for sucesor,distancia in grafo[nodo_actual]:
 
-        nodo_actual = cola_de_busqueda.pop(0) #Se saca el nodo actual
-
-        if nodo_actual not in nodos_visitados: #Si el nodo sacado no se ha visitado, 
-            nodos_visitados.append(nodo_actual)
-        
-        for ciudad, lista_adyacentes in grafo[nodo_actual]:
-            if ciudad not in nodos_visitados:
-                cola_de_busqueda.append(ciudad)
-            
-    return nodos_visitados
-
-if __name__ == '__main__':
-    #cola_resultado = busqueda_en_amplitud(Grafo,'Arad')
-    #print(len(cola_resultado))
-    #for nodo in cola_resultado:
-    #    print(nodo)
-    mi_arbol = Arbol('pipo')
-    
-    mi_arbol.añadir_hijo('hijo','pipo')
-
-    print(mi_arbol.hijos[0].valor)
-    print('done')
+      if sucesor not in visitados:
+         visitados.append(sucesor)
+         cola.append(sucesor)
+      if sucesor == meta:
+        encontrado = True
+        break
